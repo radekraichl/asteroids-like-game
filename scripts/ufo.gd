e104077b84925d99c633282068e6f440e20f24f8
Radek <radekraichl@gmail.com>
2026-01-28 19:32:15 +0100

    Add UFO

--- scripts/ufo.gd
+++ scripts/ufo.gd
@@ -1,24 +1,8 @@
 extends Node2D
 
-@export var speed_x: float = 65.0
-@export var speed_y: float = 65.0
-@export var amplitude: float = 4.0
-@export var frequency: float = 0.8
-
-var base_pos: Vector2
-var t: float = 0.0
-
-func _ready() -> void:
-	base_pos = position
+@export var speed_x: float = 25.0
+@export var speed_y: float = 15.0
 
 func _process(delta: float) -> void:
-	t += delta
-
-	# základní lineární pohyb
-	base_pos.x += speed_x * delta
-	#base_pos.y += speed_y * delta
-
-	# finální pozice = základ + kmitání
-	position.x = base_pos.x
-	position.y = base_pos.y
-	position.y = base_pos.y + sin(t * TAU * frequency) * amplitude
+	position.x += speed_x * delta
+	position.y += speed_y * delta
