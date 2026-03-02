7407835aaeaedb0bc0cbbc074cc3d026abc806cb
Radek <radekraichl@gmail.com>
2026-03-02 13:40:06 +0100

    Improve UFO shield visuals

--- scripts/shield/shield.gd
+++ scripts/shield/shield.gd
@@ -1,11 +1,13 @@
 extends AnimatedSprite2D
 
+@export var shield_color: Color = Color("0f73d2ff")
 @export var impact_color: Color = Color("1c8ffdff")
 @export var particles_color: Color = Color("41a2ffff")
 @onready var collision_shape: CollisionShape2D = %CollisionShape
 var missile_impact: PackedScene = preload("res://scenes/projectile/projectile_impact.tscn")
 
 func _ready() -> void:
+	modulate = shield_color
 	set_active(true)
 
 func set_active(active: bool) -> void:
@@ -16,5 +18,6 @@ func hit(hit_info: HitInfo):
 	var impact := missile_impact.instantiate()
 	impact.color = impact_color
 	impact.particles_color = particles_color
-	impact.position = to_local(hit_info.position)
-	add_child(impact)
+
+	impact.global_position = hit_info.position
+	get_tree().current_scene.add_child(impact)
