1846454b1667bff4fb938bc97e0a6b75715af020
Radek <radekraichl@gmail.com>
2026-04-14 20:03:32 +0200

    Add FSM

---
+++ scripts/fsm/base_state.gd
@@ -0,0 +1,29 @@
+class_name BaseState
+extends Node
+
+var state_machine: StateMachine
+var actor: Node
+
+func enter(_msg: Dictionary = {}) -> void:
+	pass
+
+func exit() -> void:
+	pass
+
+func update(_delta: float) -> void:
+	pass
+
+func physics_update(_delta: float) -> void:
+	pass
+
+func input(_event: InputEvent) -> void:
+	pass
+
+func unhandled_input(_event: InputEvent) -> void:
+	pass
+
+func unhandled_key_input(_event: InputEvent) -> void:
+	pass
+
+func transition_to(state_script: GDScript, msg: Dictionary = {}) -> void:
+	state_machine.transition_to(state_script, msg)
---
+++ scripts/fsm/state_machine.gd
@@ -0,0 +1,44 @@
+class_name StateMachine
+extends Node
+
+@export var initial_state: BaseState
+var current_state: BaseState
+var states: Dictionary = {}
+
+func _ready() -> void:
+	for child in get_children():
+		if child is BaseState:
+			states[child.get_script()] = child
+			child.state_machine = self
+			child.actor = get_parent()
+	if initial_state:
+		transition_to(initial_state.get_script())
+
+func _process(delta: float) -> void:
+	if current_state:
+		current_state.update(delta)
+
+func _physics_process(delta: float) -> void:
+	if current_state:
+		current_state.physics_update(delta)
+
+func _input(event: InputEvent) -> void:
+	if current_state:
+		current_state.input(event)
+
+func _unhandled_input(event: InputEvent) -> void:
+	if current_state:
+		current_state.unhandled_input(event)
+
+func _unhandled_key_input(event: InputEvent) -> void:
+	if current_state:
+		current_state.unhandled_key_input(event)
+
+func transition_to(state_script: GDScript, msg: Dictionary = {}) -> void:
+	if not states.has(state_script):
+		push_error("StateMachine: state not found: %s" % state_script.resource_path)
+		return
+	if current_state:
+		current_state.exit()
+	current_state = states[state_script]
+	current_state.enter(msg)
